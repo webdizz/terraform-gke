fe5c85ed122149960dec5c80a51e377a29dd1988
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-12-18 20:54:25 +0200

    Make jenkins cluster-admin

--- k8s/provider.tf
+++ k8s/provider.tf
@@ -24,6 +24,42 @@ resource "kubernetes_namespace" "ci-cd" {
 
 resource "kubernetes_service_account" "jenkins" {
   metadata {
-    name = "jenkins"
+    name      = "jenkins"
+    namespace = "${kubernetes_namespace.ci-cd.metadata.0.name}"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "jenkins-sa-cluster-admin" {
+  metadata {
+    name = "jenkins-sa-cluster-admin"
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "${kubernetes_service_account.jenkins.metadata.0.name}"
+    namespace = "${kubernetes_namespace.ci-cd.metadata.0.name}"
+    api_group = ""
+  }
+}
+resource "kubernetes_cluster_role_binding" "jenkins-sa-" {
+  metadata {
+    name = "setup-ci-cd-permissions"
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "${kubernetes_service_account.jenkins.metadata.0.name}"
+    namespace = "${kubernetes_namespace.ci-cd.metadata.0.name}"
+    api_group = ""
   }
 }
