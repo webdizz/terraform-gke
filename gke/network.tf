f74d4aa5dc165ea95ea0979a9da8762fe95f2917
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-12-19 15:07:41 +0200

    Add Cloud NAT capabilities to enable k8s private nodes/pods to communicate with external world

---
+++ gke/network.tf
@@ -0,0 +1,30 @@
+resource "google_compute_network" "default" {
+  provider                = "google-beta"
+  name                    = "${var.network_name}"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "default" {
+  provider                 = "google-beta"
+  name                     = "${var.network_name}-sub"
+  ip_cidr_range            = "10.127.0.0/20"
+  network                  = "${google_compute_network.default.self_link}"
+  region                   = "${var.region}"
+  private_ip_google_access = true
+}
+
+resource "google_compute_router" "gke-nat-router" {
+  provider = "google-beta"
+  name     = "gke-nat-router"
+  region   = "${var.region}"
+  network  = "${google_compute_network.default.self_link}"
+}
+
+resource "google_compute_router_nat" "default" {
+  provider                           = "google-beta"
+  name                               = "gke-private-nat"
+  router                             = "${google_compute_router.gke-nat-router.name}"
+  region                             = "${var.region}"
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
+}
--- gke/provider.tf
+++ gke/provider.tf
@@ -4,18 +4,3 @@ provider "google-beta" {
   project     = "${var.project_name}"
   region      = "${var.region}"
 }
-
-resource "google_compute_network" "default" {
-  provider                = "google-beta"
-  name                    = "${var.network_name}"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "default" {
-  provider                 = "google-beta"
-  name                     = "${var.network_name}"
-  ip_cidr_range            = "10.127.0.0/20"
-  network                  = "${google_compute_network.default.self_link}"
-  region                   = "${var.region}"
-  private_ip_google_access = true
-}
