58bee3598fbcff33a454d41d81943c2a3efc266f
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-12-21 10:46:57 +0200

    Change CLoud NAT router parameters to allow private nodes to connect to external world

--- gke/network.tf
+++ gke/network.tf
@@ -26,5 +26,6 @@ resource "google_compute_router_nat" "default" {
   router                             = "${google_compute_router.gke-nat-router.name}"
   region                             = "${var.region}"
   nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  # source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
 }
